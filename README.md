6117487d5785e0a69294a8103f065f93cd154ad2
Semyon Kirekov <semyon@kirekov.com>
2022-06-02 22:34:36 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # e2e-tests-example
-An example project of implementing and running E2E-tests during CI pipeline execution
+
+An example of project implementing and running E2E-tests during CI pipeline execution
---
+++ e2e-tests/src/main/java/com/e2e/tests/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.e2e.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
