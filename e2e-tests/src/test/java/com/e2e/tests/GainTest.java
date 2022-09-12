f23fda29173a33fb7ee93b0a60d207990ee22769
Semyon Kirekov <semyon@kirekov.com>
2022-09-12 21:23:06 +0300

    fix msisdn

--- e2e-tests/src/test/java/com/e2e/tests/GainTest.java
+++ e2e-tests/src/test/java/com/e2e/tests/GainTest.java
@@ -1,15 +1,18 @@
 package com.e2e.tests;
 
 import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.testcontainers.shaded.org.awaitility.Durations.FIVE_SECONDS;
 
 import com.e2e.tests.util.E2ESuite;
 import java.util.Map;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
 class GainTest extends E2ESuite {
 
   @Test
+  @SneakyThrows
   void shouldGainMessage() {
     rest.post(
         "/api/message",
@@ -26,6 +29,13 @@ class GainTest extends E2ESuite {
             "cookie", "cookie-value",
             "msisdn", "88005553535"
         )));
+
+    Thread.sleep(5000);
+    assertTrue(getGainQueueMessages().contains(Map.of(
+        "some_key", "some_value",
+        "cookie", "cookie-value",
+        "msisdn", "88005553535"
+    )));
 
     rest.post(
         "/api/message",
